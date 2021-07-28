943e7718ead6d81c8f5577d320fd6d365a7844f1
carlosviana <viana.mail@gmail.com>
2021-07-28 17:45:49 -0300

    testes

--- lib/explorando_marte.ex
+++ lib/explorando_marte.ex
@@ -2,6 +2,75 @@ defmodule ExplorandoMarte do
   @moduledoc """
   Documentation for `ExplorandoMarte`.
   """
+  alias ExplorandoMarte.DirecaoCardinal
 
+  def script_sonda(area, posicao, comandos) do
+    comandos = String.graphemes(comandos)
 
+    for comando <- comandos do
+      cond do
+        comando == "L" || comando == "R" ->
+          muda_posicao(comando, posicao)
+
+        comando == "M" ->
+          movimenta(posicao, area)
+
+        true ->
+          comando
+      end
+    end
+  end
+
+  def muda_posicao(comando, posicao) do
+    nova_posicao =
+      posicao
+      |> String.graphemes()
+      |> Enum.reverse()
+      |> hd()
+      |> DirecaoCardinal.get(comando)
+
+    posicao
+    |> String.graphemes()
+    |> Enum.reverse()
+    |> tl()
+    |> Enum.reverse()
+    |> Enum.concat([nova_posicao])
+    |> List.to_string()
+  end
+
+  def movimenta(posicao, area) do
+    pos = String.split(posicao)
+
+    area = %{:x => Enum.at(area, 0), :y => Enum.at(area, 0)}
+
+    pos = %{
+      :x => Enum.at(pos, 0) |> String.to_integer(),
+      :y => Enum.at(pos, 1) |> String.to_integer(),
+      :direcao => Enum.at(pos, 2)
+    }
+
+    xs =
+      cond do
+        pos[:direcao] == "W" && pos[:x] > 0 ->
+          %{pos | :x => pos[:x] - 1}
+
+        pos[:direcao] == "E" && pos[:x] < area[:x] ->
+          %{pos | :x => pos[:x] + 1}
+
+        pos[:direcao] == "N" && pos[:y] < area[:y] ->
+          %{pos | :y => pos[:y] + 1}
+
+        pos[:direcao] == "S" && pos[:y] > 0 ->
+          %{pos | :y => pos[:y] - 1}
+
+        true ->
+          pos
+      end
+
+    x = xs[:x] |> Integer.to_string()
+    y = xs[:y] |> Integer.to_string()
+    dir = xs[:direcao]
+
+    x <> " " <> y <> " " <> dir
+  end
 end
--- test/explorando_marte_test.exs
+++ test/explorando_marte_test.exs
@@ -2,5 +2,27 @@ defmodule ExplorandoMarteTest do
   use ExUnit.Case
   doctest ExplorandoMarte
 
+  test "mudando de posicao no script" do
+    assert ExplorandoMarte.script_sonda([5, 5], "1 2 N", "L") == ["1 2 W"]
+  end
 
+  test "movimento unico dentro do script" do
+    assert ExplorandoMarte.script_sonda([5, 5], "1 2 W", "M") == ["0 2 W"]
+  end
+
+  test "movimentos completos" do
+    assert ExplorandoMarte.script_sonda([5, 5], "1 2 N", "LMLMLMLMM") |> List.last() == "1 3 N"
+    assert ExplorandoMarte.script_sonda([5, 5], "3 3 E", "MMRMMRMRRM") == "5 1 E"
+  end
+
+  test "mudando posicao" do
+    assert ExplorandoMarte.muda_posicao("L", "N") == "W"
+    assert ExplorandoMarte.muda_posicao("R", "N") == "E"
+    assert ExplorandoMarte.muda_posicao("L", "S") == "E"
+    assert ExplorandoMarte.muda_posicao("R", "S") == "W"
+    assert ExplorandoMarte.muda_posicao("L", "E") == "N"
+    assert ExplorandoMarte.muda_posicao("R", "E") == "S"
+    assert ExplorandoMarte.muda_posicao("L", "W") == "S"
+    assert ExplorandoMarte.muda_posicao("R", "W") == "N"
+  end
 end
